e493c56a2b5be7d246ac72b21e7f7ecece1121e6
Ethan Cornwell <ethan.aubrey22@gmail.com>
2022-12-14 09:19:07 -0700

    functioning nav

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { useLocation, Link} from 'react-router-dom';
+
+
+const Nav = (props) => {
+  const products = props.products;
+  const location = useLocation();
+  const pathname = location.pathname;
+  return (
+    <nav>
+    <Link to='/' className={ pathname === '/' ? 'selected' : '' }>Home</Link>
+    <Link to='/products' className={ pathname.startsWith('/products') ? 'selected' : ''}>Products ({ products.length })</Link>
+  </nav>
+  );
+};
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import React, { useState, useEffect } from 'react';
-import { useParams, HashRouter, Routes, Route, Link} from 'react-router-dom';
+import { useLocation, useParams, HashRouter, Routes, Route, Link} from 'react-router-dom';
+import Products from './Products';
+import Product from './Product';
+import Nav from './Nav';
 
-const Product = (props)=> {
-  const products = props.products
-  const id = useParams().id;
-  const product = products.find(product => product.id === id)
-  if(!product){
-    return null;
-  }
-  return (
-    <div>
-      <h1><Link to='/products'>{ product.name }</Link></h1>
-      <p>
-        {product.description}
-      </p>
-    </div>
-  );
-}
-const Products = (props) => {
-  const products = props.products;
-  return (
-    <div>
-      <h1>Products</h1>
-      <ul>
-        {
-          products.map(product => {
-            return (
-              <li key={product.id}>
-                <Link to={`/products/${product.id}`}>{ product.name }</Link>
-              </li>
-            );
-          })
-        }
-      </ul>
-    </div>
-  )
-}
+
+
 const App = ()=> {
   //https://www.acme-api.com/api/products
   const [products, setProducts] = useState([])
@@ -48,12 +18,16 @@ const App = ()=> {
   return (
     <div>
       <h1>React Client Template</h1>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/products'>Products ({ products.length })</Link>
-      </nav>
       <Routes>
-        <Route path='/' element= { <div>Home</div> } />} />
+        <Route
+          path='/*'
+          element = {
+            <Nav products={ products } />
+          }
+        />
+      </Routes> 
+      <Routes>
+        <Route path='/' element= { <div>Home</div> } />
         <Route path='/products' element={
           <Products products={ products }/>
         } />
